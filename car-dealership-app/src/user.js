0e3546d418b7f3ba413cf34e57c82fb6020559db
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-12-16 16:38:19 -0500

    add first test

--- car-dealership-app/src/user.js
+++ car-dealership-app/src/user.js
@@ -1,32 +1,7 @@
-/*Group 2: Introduce the ability to register a user.
-As a user, I can register as a customer with a starting amount of money.
-*/
-
 import fs from 'fs';
 
 import * as users from './util/users.json';
 
-export function getUser(username) {
-  return users.find((person) => person.name === username);
-}
-export function login(user, password) {
-  console.log(users);
-  return users.find(
-    (person) => person.name === user && person.pass === password
-  );
-}
-
-export function register(username, password, money) {
-  users.push({
-    name: username,
-    pass: password,
-    money: money,
-    role: 'Customer',
-  });
-  //TO-DO: registration for employee
-}
-
-export function saveUsers() {
-  let u = JSON.stringify(users);
-  fs.writeFileSync('users.json', u);
+export function getUsers() {
+  return users;
 }
